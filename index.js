3464188aeecbdce539834818421ddb3121180212
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-08-12 16:29:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(array, string){
+    return array.filter(x => x.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(x => x.charAt(0) === string.charAt(0))
+}
+
+function matchName(array, string){
+    return array.filter(x => x.name == string)
+}
