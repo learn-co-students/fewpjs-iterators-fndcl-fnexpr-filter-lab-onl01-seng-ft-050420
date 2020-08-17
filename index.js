5ec7317030c5ffa997854216005832cd2d0e4014
kikuishimoto <kikuish@gmail.com>
2020-08-17 15:41:26 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter( find_name => {
+      return (find_name.toLowerCase() === name.toLowerCase())
+     }
+    )
+  }
+
+  function fuzzyMatch(drivers, letter){
+    return drivers.filter( find_name => {
+      if (letter.charAt(0) === find_name.charAt(0)){
+      return find_name
+      }
+    })
+  }
+
+function matchName(drivers, string) {
+    return drivers.filter( find_name => {
+        if (find_name.name === string) {
+            return find_name
+        }
+    })
+
+}
